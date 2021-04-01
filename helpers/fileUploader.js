609eac59219393f4d5d22fb0215cc8cb3d00a4c3
nghiavt2906 <nghiavt2906@gmail.com>
2021-04-01 22:42:15 +0700

    version 2.8

--- helpers/fileUploader.js
+++ helpers/fileUploader.js
@@ -1,6 +1,18 @@
 const multer = require('multer')
+const fs = require('fs')
 
 const createFileUploader = path => {
+    if (!fs.existsSync(__dirname + '\\..\\media')) {
+        fs.mkdirSync(__dirname + '\\..\\media');
+    }
+
+    let parts = path.split('/')
+    parts.shift()
+
+    let modifiedPath = __dirname + "\\..\\" + parts.join("\\")
+    if (!fs.existsSync(modifiedPath)) {
+        fs.mkdirSync(modifiedPath);
+    }
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
             cb(null, path)
